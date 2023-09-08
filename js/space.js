69dfb16c39e497a3f7b4c568c511d9ba238cdc62
Gustavo <141040266+GustavoFiguerido1010@users.noreply.github.com>
2023-09-08 09:02:13 -0300

    Add files via upload

---
+++ js/space.js
@@ -0,0 +1,63 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const inputBuscar = document.getElementById("inputBuscar");
+    const btnBuscar = document.getElementById("btnBuscar");
+    const btnBorrar = document.getElementById("btnBorrar");
+    const contenedor = document.getElementById("contenedor");
+    const listaImagenes = document.getElementById("listaImagenes");
+  
+    btnBuscar.addEventListener("click", function () {
+      const query = inputBuscar.value.trim();
+  
+      if (query !== "") {
+        const apiUrl = `https://images-api.nasa.gov/search?q=${query}`;
+  
+        fetch(apiUrl)
+          .then((response) => response.json())
+          .then((data) => {
+            // Limpia el contenedor antes de mostrar nuevos resultados
+            listaImagenes.innerHTML = "";
+  
+            if (data.collection && data.collection.items.length > 0) {
+              data.collection.items.forEach((item) => {
+                if (item.links && item.links.length > 0) {
+                  const imageUrl = item.links[0].href;
+                  const title = item.data[0].title || "Sin título";
+                  const description = item.data[0].description || "Sin descripción";
+                  const dateCreated = item.data[0].date_created || "Sin fecha";
+  
+                  const listItem = document.createElement("li"); // Crea un elemento de lista
+                  const img = document.createElement("img");
+                  img.src = imageUrl;
+  
+                  // Crea un div para mostrar la información de la imagen
+                  const infoDiv = document.createElement("div");
+                  infoDiv.className = "imagen-info";
+                  infoDiv.innerHTML = `
+                    <h2>${title}</h2>
+                    <p>${description}</p>
+                    <p>Fecha: ${dateCreated}</p>
+                  `;
+  
+                  // Agrega el elemento de imagen y el div de información a la lista de imágenes
+                  listItem.appendChild(img);
+                  listItem.appendChild(infoDiv);
+                  listaImagenes.appendChild(listItem); // Agrega el elemento de lista a la lista ordenada
+                }
+              });
+            } else {
+              listaImagenes.innerHTML = "No se encontraron imágenes.";
+            }
+          })
+          .catch((error) => {
+            console.error("Error al buscar imágenes:", error);
+          });
+      }
+    });
+    
+  // borrar busqueda
+    btnBorrar.addEventListener("click", function () {
+      inputBuscar.value = "";
+      listaImagenes.innerHTML = "";
+    });
+  });
+  
